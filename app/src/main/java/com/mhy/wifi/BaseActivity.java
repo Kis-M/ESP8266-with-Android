b999f3caf89f97c161acc0f73d3219dc039fdb2f
mahaoyong <1902006704@qq.com>
2021-05-20 17:12:02 +0800

    5.20

---
+++ app/src/main/java/com/mhy/wifi/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.mhy.wifi;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
